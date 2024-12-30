dbc30f7bf9ce93ee62259eea32d8513dbead9f38
Csab-PC <csabi@csabi.zip>
2024-12-30 03:21:38 +0100

    rework

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const snowContainer = document.getElementById("snow-container");
-
-        function createSnowflake() {
-            const snowflake = document.createElement("div");
-            snowflake.classList.add("snowflake");
-            
-            // Randomize size and position
-            const size = Math.random() * 5 + 2; // Size between 2px and 7px
-            snowflake.style.width = `${size}px`;
-            snowflake.style.height = `${size}px`;
-            
-            // Random horizontal position
-            snowflake.style.left = `${Math.random() * 100}vw`;
-            
-            // Add fall animation
-            snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; // Duration between 2s and 5s
-            snowflake.style.animationName = 'fall';
-            
-            // Append to container
-            snowContainer.appendChild(snowflake);
-            
-            // Remove snowflake after animation ends
-            snowflake.addEventListener('animationend', () => {
-                snowflake.remove();
-            });
-        }
-
-        // Create snowflakes at intervals
-        setInterval(createSnowflake, 25);
+VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
+    max: 10,
+    speed: 10,
+    glare: true,
+    "max-glare": 1
+});
